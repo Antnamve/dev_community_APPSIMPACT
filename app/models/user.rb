0787409885df6fe6f5ca1223fdfa50727edc91df
Antnamve <antnamve@gmail.com>
2023-12-23 00:07:51 +0300

    up to lesson 6

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :trackable
+
+  PROFILE_TITLE = [
+    'Senior Ruby on Rails Developer',
+    'Junior Full Stack Ruby on Rails Developer',
+    'Senior Full Stack Ruby on Rails Developer',
+    'Senior Java Developer',
+    'Senior Front End Developer'
+  ].freeze
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+end
