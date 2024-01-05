136a4179268627be4c99a33589a9ade838f4c9d5
Antnamve <antnamve@gmail.com>
2024-01-05 04:45:55 +0300

    up to lesson 26

---
+++ app/controllers/work_experiences_controller.rb
@@ -0,0 +1,28 @@
+class WorkExperiencesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_work_experience, only %i[edti update destroy]
+
+  def new
+    @work_experience = current_user.work_experiences.new
+  end
+
+  def edit; end
+
+  def create; end
+
+  def destroy; end
+
+  private
+
+  def set_work_experience
+    @work_experience = current_user.work_experiences.find(params[:id])
+  end
+
+  def work_experience_params
+    params.require(:work_experience)
+    .permit(:start_date, :end_date, :currently_working_here, 
+      :company, :employment_type, :location, :location_type, 
+      :description, :user_id, :job_title
+    )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_231_222_200_501) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_04_203322) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer 'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.string 'first_name'
-    t.string 'last_name'
-    t.date 'date_of_birth'
-    t.string 'username'
-    t.string 'city'
-    t.string 'state'
-    t.string 'country'
-    t.string 'pincode'
-    t.string 'street_address'
-    t.string 'profile_title'
-    t.text 'about'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'contact_number'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "first_name"
+    t.string "last_name"
+    t.date "date_of_birth"
+    t.string "username"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "pincode"
+    t.string "street_address"
+    t.string "profile_title"
+    t.text "about"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "contact_number"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  create_table "work_experiences", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "job_title"
+    t.string "employment_type"
+    t.string "location"
+    t.string "location_type"
+    t.boolean "currently_working_here"
+    t.text "description"
+    t.string "company"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_work_experiences_on_user_id"
+  end
+
+  add_foreign_key "work_experiences", "users"
 end
