d25f65e4a08a7b97443a0e1fae4451d7f9dfa29b
Antnamve <antnamve@gmail.com>
2024-01-08 01:01:35 +0300

    up to lesson 36

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
-  has_many :work_experiences
+  has_many :work_experiences, dependent: :destroy
+  has_many :conections, dependent: :destroy
 
   PROFILE_TITLE = [
     'Senior Ruby on Rails Developer',
@@ -28,5 +29,9 @@ class User < ApplicationRecord
 
   def self.ransackable_associations(_auth_object = nil)
     []
+  end
+
+  def check_if_already_connected?(current_user, user)
+    current_user != user && !current_user.conections.pluck(:connected_user_id).include?(user.id)
   end
 end
